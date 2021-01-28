03ffb27fb951a9c666c9a7a7ca0b3287bbaa7ab8
Kate <1502225@rgu.ac.uk>
2021-01-28 15:25:35 +0000

    kl

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
   echo '<p> Hello World from zennith</p>';
-  echo '<a href="Magic Hate Ball/hate_ball_frontend.html">The Magic Hate Ball! </a>';'
+  echo '<a href="hate_ball_frontend.html">The Magic Hate Ball! </a>';'
   ?>
